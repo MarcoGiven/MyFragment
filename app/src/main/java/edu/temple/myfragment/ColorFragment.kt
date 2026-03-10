90760279e055d4ca33b361713b15851fb958f18a
MarcoGiven <marcogiven15@gmail.com>
2026-03-10 11:51:00 -0400

    Added fragment

---
+++ app/src/main/java/edu/temple/myfragment/ColorFragment.kt
@@ -0,0 +1,29 @@
+package edu.temple.myfragment
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ColorFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+            setOnClickListener {
+                setBackgroundColor(Color.parseColor(
+                    colorString = arrayOf("Red", "Green", "Magenta", "Grey", "Yellow").random()
+                ))
+            }
+        }
+    }
+
+
+}
